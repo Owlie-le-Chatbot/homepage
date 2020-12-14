44ded59319d52ea721bce8d9b8af79bbc295dba6
François Falala-Sechet <francois@clevy.io>
2020-12-14 09:40:10 +0100

    fix: full height in mobile without scroll

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="h-screen w-screen">
-    <chat-iframe />
-  </div>
+  <chat-iframe class="full-height w-screen" />
 </template>
+
+<style lang="css" scoped>
+.full-height {
+  min-height: 100vh;
+  /* fix for mobile full height without scroll */
+  /* https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit/ */
+  min-height: -webkit-fill-available;
+}
+</style>
