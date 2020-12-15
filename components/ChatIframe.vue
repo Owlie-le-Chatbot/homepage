5dc79ec483fd5b46a31dd48eb98fccd31d3b168f
François Falala-Sechet <francois@clevy.io>
2020-12-15 14:58:35 +0100

    imp: make debug mode optional

--- components/ChatIframe.vue
+++ components/ChatIframe.vue
@@ -1,8 +1,23 @@
 <template>
   <iframe
-    src="https://chat.csml.dev/channels/xxneg4neyafrlgczyqyl3iikmqhezxwm?debug=true"
+    :src="`https://chat.csml.dev/channels/xxneg4neyafrlgczyqyl3iikmqhezxwm?debug=${debug_mode}`"
     width="100%"
     height="100%"
     frameborder="0"
   ></iframe>
 </template>
+
+<script>
+export default {
+  data() {
+    let debug_mode = 'false';
+    if (process.client) {
+      const q = new URLSearchParams(window.location.search);
+      debug_mode = q.get('debug') || 'false';
+    }
+    return {
+      debug_mode,
+    };
+  },
+};
+</script>
