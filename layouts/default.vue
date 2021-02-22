9c0963660bc6802234bc9e6719010c556fe7ad77
Mathieu Morainville <mathieu.morainville@clevy.io>
2021-02-22 15:28:53 +0100

    fix: make the iframe full height (while keeping webkit-fill-available)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,20 @@
 <template>
   <Nuxt id="app" />
 </template>
+
+<style>
+body {
+  min-height: 100vh;
+  /* mobile viewport bug fix */
+  min-height: -webkit-fill-available;
+  display: flex;
+}
+
+html {
+  height: -webkit-fill-available;
+}
+
+#__nuxt, #__layout {
+  min-height: 100%;
+}
+</style>
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,12 +1,3 @@
 <template>
-  <chat-iframe class="full-height w-screen" />
+  <chat-iframe class="absolute h-full w-screen" />
 </template>
-
-<style lang="css" scoped>
-.full-height {
-  min-height: 100vh;
-  /* fix for mobile full height without scroll */
-  /* https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit/ */
-  min-height: -webkit-fill-available;
-}
-</style>
